477927061e9601076664eaf1f7784546ae13fc22
Brian Hoffman <brianjhoffman@gmail.com>
2013-09-25 14:50:07 -0400

    refine home page layout and resize videos

--- lib/themes/manhattanville/page.tpl.php
+++ lib/themes/manhattanville/page.tpl.php
@@ -57,18 +57,21 @@
 
 <?php $body_class = (!$page['sidebar_first'] ? 'pure-u-3-4' : 'pure-u-1-2'); ?>
 
-    <div class="<?php print $body_class; ?> content">
+    <div class="<?php print $body_class; ?> body-content">
 
        <div class="element-invisible"><a id="main-content"></a></div>
         <?php if (isset($trail_title)) : ?>
            <h2 class="title"><?php print $trail_title ?></h2>
          <?php endif; ?>
+   		<div class="body-content-inner">
         <?php print render($page['content']); ?>
-
+   		</div>
     </div>
    <?php if ($page['sidebar_first']) : ?>
 	   	<div class="pure-u-1-2 sidebar-right">
-   			<?php print render($page['sidebar_first']); ?>
+       <div class="sidebar-inner">
+     		<?php print render($page['sidebar_first']); ?>
+   		</div>
  			</div>
    <?php endif; ?>
 
